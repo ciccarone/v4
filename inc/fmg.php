99c069ad65b4be4e52c92919ba4d641f7981798c
Tony Ciccarone <tonyvciccarone@gmail.com>
2021-07-14 21:33:16 -0400

    Retrieve color (function) for header from ACF options

--- inc/fmg.php
+++ inc/fmg.php
@@ -1,9 +1,5 @@
 <?php
-add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
-function my_theme_enqueue_styles() {
-    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
-    wp_enqueue_style( 'ciccarone-style', get_stylesheet_directory_uri() . '/style.css' );
-}
+
 if( function_exists('acf_add_options_page') ) {
 
 	acf_add_options_page();
@@ -490,3 +486,12 @@ function company_email()
   }
 }
 add_shortcode('company_email', 'company_email');
+
+
+
+function fmg_retrieve_color($field)
+{
+  if ($color = get_field($field, 'option')) {
+    return $color['color_selector'];
+  }
+}
