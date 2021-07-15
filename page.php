24f9284f3a46e17ad068425e14abe49ca741e229
Tony Ciccarone <tonyvciccarone@gmail.com>
2021-07-14 20:51:51 -0400

    Begin basic page section loop

--- page.php
+++ page.php
@@ -21,8 +21,15 @@ get_header();
 		while ( have_posts() ) :
 			the_post();
 
-			if (condition) {
-				// code...
+			if ($page_sections = get_field('page_sections')) {
+				// var_dump($page_sections);
+				foreach ($page_sections as $page_section) {
+					echo '<section class="page-section page-section--'.$page_section['acf_fc_layout'].'">';
+					include( locate_template( 'template-parts/fmg-'.$page_section['acf_fc_layout'].'.php', false, false ) );
+
+					echo '</section>';
+
+				}
 			}
 
 		endwhile; // End of the loop.
