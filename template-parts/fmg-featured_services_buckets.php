e90283ee841118f96cad96a417ec509402ec3a6c
Tony Ciccarone <tonyvciccarone@gmail.com>
2021-07-16 08:21:09 -0400

    Cards partial and buckets (containing cards)

---
+++ template-parts/fmg-featured_services_buckets.php
@@ -0,0 +1,3 @@
+<div class="container">
+  <?php echo do_shortcode('[featured_services_rows service_type="marketing_service" bucket_type="'.$page_section['featured_services_buckets_graphic'].'"]') ?>
+</div>
