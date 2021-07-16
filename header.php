e5a2743b252482d0b6468b636e4a134271e32d7f
Tony Ciccarone <tonyvciccarone@gmail.com>
2021-07-16 07:08:09 -0400

    Add dynamic toggle variations

--- header.php
+++ header.php
@@ -34,7 +34,7 @@
 						the_custom_logo();
 						?>
 					</div><!-- .site-branding -->
-			    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+			    <button class="navbar-toggler custom-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 			      <span class="navbar-toggler-icon"></span>
 			    </button>
 			    <div class="collapse navbar-collapse flex-grow-1 text-right" id="navbarSupportedContent">
