4abe1540ef62aac7216c2c03a75813f667c64f3b
Tony Ciccarone <tonyvciccarone@gmail.com>
2021-08-03 20:24:21 -0400

    Remove that container (for now)

--- template-parts/fmg-featured_services_ping_pong.php
+++ template-parts/fmg-featured_services_ping_pong.php
@@ -1,3 +1,3 @@
-<div class="container">
+<!-- <div class="container"> -->
   <?php echo do_shortcode('[featured_services_ping_pong service_type="marketing_service" bucket_type="'.$page_section['featured_services_buckets_graphic'].'"]') ?>
-</div>
+<!-- </div> -->
