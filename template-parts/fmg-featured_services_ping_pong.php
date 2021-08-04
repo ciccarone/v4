2047728c4ce397161aa90ed428cce255493efafa
Tony Ciccarone <tonyvciccarone@gmail.com>
2021-08-03 20:51:41 -0400

    Just don't..

--- template-parts/fmg-featured_services_ping_pong.php
+++ template-parts/fmg-featured_services_ping_pong.php
@@ -1,3 +1,1 @@
-<!-- <div class="container"> -->
-  <?php echo do_shortcode('[featured_services_ping_pong service_type="marketing_service" bucket_type="'.$page_section['featured_services_buckets_graphic'].'"]') ?>
-<!-- </div> -->
+<?php echo do_shortcode('[featured_services_ping_pong service_type="marketing_service" bucket_type="'.$page_section['featured_services_buckets_graphic'].'"]') ?>
