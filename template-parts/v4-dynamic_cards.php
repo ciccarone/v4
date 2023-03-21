fafe153164221148af6e3d7a3b34f8c1398faea1
Tony Ciccarone <tonyvciccarone@gmail.com>
2023-03-21 11:07:14 -0400

    reorganize ACF

--- template-parts/v4-dynamic_cards.php
+++ template-parts/v4-dynamic_cards.php
@@ -1,20 +1,20 @@
 <div>
   <?php
-    echo $section_headings;
-    echo $section_texts;
-    $cards['count'] = $page_section['card_count'];
-    $cards['query'] = $page_section['card_query'];
-    $cards['category'] = $page_section['card_category'];
-    $cards['category_condition'] = $page_section['card_category_condition'];
-    $cards['border_radius'] = $page_section['card_border_radius'] ? 'global_border_radius' : '';
+  echo $section_headings;
+  echo $section_texts;
+  $cards['count'] = $page_section['card_count'];
+  $cards['query'] = $page_section['card_query'];
+  $cards['category'] = $page_section['card_category'];
+  $cards['type'] = $page_section['card_type'];
+  $cards['type_class'] = $page_section['card_type'] == 'full' ? 'v4-index__layout--full' : '';
+  $cards['category_condition'] = $page_section['card_category_condition'];
+  $cards['border_radius'] = $page_section['card_border_radius'] ? 'global_border_radius' : '';
   ?>
 
-  <div class="v4-cards v4-cards__count--<?php echo $cards['count']; ?> grid">
+  <div class="v4-cards v4-cards__count--<?php echo $cards['count']; ?> grid <?php echo $cards['type_class'];?>">
     <?php
-      echo v4_dynamic_cards($cards);
+    echo v4_dynamic_cards($cards);
     ?>
   </div>
 
 </div>
-
-
